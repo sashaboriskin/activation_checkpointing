07e85f2e7e589835b6086ec38410005d625f39ce
sashaboriskin <brskn.lx@gmail.com>
2025-10-12 22:53:08 +0000

    Add custom activation checkpointing

---
+++ activation_checkpoint.py
@@ -0,0 +1,93 @@
+import torch
+from torch.autograd import Function
+
+
+class ActivationCheckpoint(Function):
+    @staticmethod
+    def forward(ctx, run_fn, *args):
+        ctx.run_fn = run_fn
+
+        ctx.device_type = "cuda"
+        ctx.autocast_kwargs = dict(
+            enabled=torch.is_autocast_enabled(ctx.device_type),
+            dtype=torch.get_autocast_dtype(ctx.device_type),
+            cache_enabled=torch.is_autocast_cache_enabled(),
+        )
+
+        # tensor/not_tensor data
+        ctx.tensor_idx = []
+        ctx.nontensor_args = {}
+        tensor_args = []
+        for i, a in enumerate(args):
+            if torch.is_tensor(a):
+                ctx.tensor_idx.append(i)
+                tensor_args.append(a)
+            else:
+                ctx.nontensor_args[i] = a
+
+        # save only tensors for backward
+        ctx.save_for_backward(*tensor_args)
+
+        # forward without activations
+        with torch.no_grad():
+            out = run_fn(*args)
+        return out
+
+    @staticmethod
+    def backward(ctx, *grad_out):
+
+        saved = list(ctx.saved_tensors)
+        args = []
+        s_it = 0
+
+        max_idx = max([*ctx.tensor_idx, *ctx.nontensor_args.keys()])
+        for i in range(max_idx + 1):
+            if i in ctx.nontensor_args:
+                args.append(ctx.nontensor_args[i])
+            else:
+                args.append(saved[s_it])
+                s_it += 1
+
+        # get the autocast_ctx
+        autocast_ctx = torch.amp.autocast(
+            device_type=ctx.device_type, **ctx.autocast_kwargs
+        )
+
+        # build a new torch graph
+        with torch.enable_grad(), autocast_ctx:
+            detached = []
+            for a in args:
+                if torch.is_tensor(a):
+                    b = a.detach()
+                    b.requires_grad = a.requires_grad
+                    detached.append(b)
+                else:
+                    detached.append(a)
+
+            # make a new forward
+            out = (ctx.run_fn(*tuple(detached)),)
+
+        # filter the outputs that require gradients
+        outs_req = []
+        grads_req = []
+        for o, g in zip(out, grad_out):
+            if torch.is_tensor(o) and o.requires_grad:
+                outs_req.append(o)
+                grads_req.append(g)
+
+        # make a new backward
+        torch.autograd.backward(outs_req, grads_req)
+
+        # collect grads in the input format
+        grad_inputs = []
+        for a in detached:
+            if torch.is_tensor(a) and a.requires_grad:
+                grad_inputs.append(a.grad)
+            else:
+                grad_inputs.append(None)
+
+        return (None, *grad_inputs)
+
+
+def checkpoint(fn, *args):
+    return ActivationCheckpoint.apply(fn, *args)
--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from flash_attn import flash_attn_varlen_qkvpacked_func
 
+from activation_checkpoint import checkpoint
+
 
 class MultiheadSelfAttention(nn.Module):
 
@@ -15,18 +17,22 @@ class MultiheadSelfAttention(nn.Module):
         self.to_value = nn.Linear(num_channels, num_channels, bias=True)
         self.query_norm = nn.RMSNorm(head_dim)
         self.key_norm = nn.RMSNorm(head_dim)
-        
+
         self.out_layer = nn.Linear(num_channels, num_channels, bias=True)
 
     def forward(self, x, cu_seqlens):
         shape = x.shape[:-1]
         query, key, value = self.to_query(x), self.to_key(x), self.to_value(x)
-        query = self.query_norm(query.reshape(*shape, self.num_heads, -1)).type_as(query)
+        query = self.query_norm(query.reshape(*shape, self.num_heads, -1)).type_as(
+            query
+        )
         key = self.key_norm(key.reshape(*shape, self.num_heads, -1)).type_as(key)
         value = value.reshape(*shape, self.num_heads, -1)
         query_key_value = torch.stack([query, key, value], dim=-3)
 
-        out = flash_attn_varlen_qkvpacked_func(query_key_value, cu_seqlens, torch.diff(cu_seqlens).max()).flatten(-2, -1)
+        out = flash_attn_varlen_qkvpacked_func(
+            query_key_value, cu_seqlens, torch.diff(cu_seqlens).max()
+        ).flatten(-2, -1)
         out = self.out_layer(out)
         return out
 
@@ -49,7 +55,7 @@ class ModelBlock(nn.Module):
         super().__init__()
         self.attention_norm = nn.RMSNorm(dim)
         self.attention = MultiheadSelfAttention(dim, head_dim)
-        
+
         self.feed_forward_norm = nn.RMSNorm(dim)
         self.feed_forward = FeedForward(dim, ff_dim)
 
@@ -60,11 +66,12 @@ class ModelBlock(nn.Module):
 
 
 class Model(nn.Module):
-
     def __init__(self, in_dim, hidden_dim, ff_dim, num_layers, head_dim):
         super().__init__()
         self.in_layer = nn.Linear(in_dim, hidden_dim)
-        self.blocks = nn.ModuleList([ModelBlock(hidden_dim, ff_dim, head_dim) for _ in range(num_layers)])
+        self.blocks = nn.ModuleList(
+            [ModelBlock(hidden_dim, ff_dim, head_dim) for _ in range(num_layers)]
+        )
         self.out_layer = nn.Linear(hidden_dim, in_dim)
 
     def forward(self, x, cu_seqlens):
@@ -72,4 +79,39 @@ class Model(nn.Module):
         for block in self.blocks:
             x = block(x, cu_seqlens)
         return self.out_layer(x)
-        
+
+
+class ModelBlockCheckpoint(nn.Module):
+    def __init__(self, dim, ff_dim, head_dim):
+        super().__init__()
+        self.attention_norm = nn.RMSNorm(dim)
+        self.attention = MultiheadSelfAttention(dim, head_dim)
+
+        self.feed_forward_norm = nn.RMSNorm(dim)
+        self.feed_forward = FeedForward(dim, ff_dim)
+
+    def forward(self, x, cu_seqlens):
+        def block_fn(x_, cu_):
+            x1 = x_ + self.attention(self.attention_norm(x_), cu_)
+            return x1 + self.feed_forward(self.feed_forward_norm(x1))
+
+        return checkpoint(block_fn, x, cu_seqlens)
+
+
+class ModelCheckpoint(nn.Module):
+    def __init__(self, in_dim, hidden_dim, ff_dim, num_layers, head_dim):
+        super().__init__()
+        self.in_layer = nn.Linear(in_dim, hidden_dim)
+        self.blocks = nn.ModuleList(
+            [
+                ModelBlockCheckpoint(hidden_dim, ff_dim, head_dim)
+                for _ in range(num_layers)
+            ]
+        )
+        self.out_layer = nn.Linear(hidden_dim, in_dim)
+
+    def forward(self, x, cu_seqlens):
+        x = self.in_layer(x)
+        for block in self.blocks:
+            x = block(x, cu_seqlens)
+        return self.out_layer(x)
